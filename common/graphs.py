45e343bba0600c7ee0f89e402712ee5416399edf
wepiha <2267001+wepiha@users.noreply.github.com>
2019-01-17 17:50:51 +1300

    Renamed fancurvegraph.py to more generic graphs.py FanCurveGraph is now EditableGraph Added ScrollingGraph which is essentially a stub with dummy data

--- common/graphs.py
+++ common/graphs.py
@@ -6,21 +6,88 @@ import pyqtgraph as pg
 import numpy as np
 import math
 
+def get_plotwidget_item(parent, name):
+    """ Helper function to acquire items added to the `PlotWidget` """
+    for item in parent.plotItem.items:
+        if (hasattr(item, '_name')) and (item._name == name):
+            return item
+    
+    raise LookupError("The item '%s' was not found" % name)
 
-class FanCurveGraph(pg.GraphItem):
+def InitPlotWidget(plotwidget, **kwds):
+    
+    kwds_default = {
+        'menuEnabled': False,
+        'aspectLocked': True,
+        'hideButtons': True,
+
+        'labels': {
+            'bottom': [],
+            'left': []
+        },
+
+        'showGrid': {
+            'x': True,
+            'y': True,
+            'alpha': 0.5
+        },
+
+        'tickSpacing' : {
+            'bottom': False,
+            'left': False
+        },
+
+        'background' : None
+    }
+
+    data = dict(kwds_default, **kwds)
+
+    plotwidget.setMenuEnabled(data['menuEnabled'])
+    plotwidget.setLabels(**data['labels'])
+    plotwidget.setAspectLocked(data['aspectLocked'])
+    plotwidget.showGrid(**data['showGrid'])
+    plotwidget.setBackground(data['background'])
+
+    if (data['tickSpacing']['bottom'] is not False):
+        plotwidget.getAxis('bottom').setTickSpacing(data['tickSpacing']['bottom'][0], data['tickSpacing']['bottom'][1])
+    
+    if (data['tickSpacing']['left'] is not False):
+        plotwidget.getAxis('left').setTickSpacing(data['tickSpacing']['left'][0], data['tickSpacing']['left'][1])
+
+    if 'hideButtons' in data:
+        plotwidget.hideButtons()
+    
+    if 'limits' in data:
+        if len(data['limits']) == 2:
+            plotwidget.setLimits(
+                xMin = data['limits'][0],
+                yMin = data['limits'][0],
+                xMax = data['limits'][1] + data['limits'][0],
+                yMax = data['limits'][1] + data['limits'][0],
+                minXRange = data['limits'][1],
+                maxXRange = data['limits'][1],
+                minYRange = data['limits'][1],
+                maxYRange = data['limits'][1]
+            )
+
+class EditableGraph(pg.GraphItem):
     MIN_POINT_DISTANCE = 16
 
     def __init__(self, parent, data, name, staticPos=None):
         pg.GraphItem.__init__(self)
 
-        self.dragPoint = None
-        self.dragOffset = None
         self.plotWidget = parent
+
+        # adds _name attribute similar tto those in plotItem.items
         self._name = name
-        
         self.staticPos = staticPos
 
+        self.dragPoint = None
+        self.dragOffset = None
+        
         self.setData(pos=np.stack(data))
+
+        parent.addItem(self)
 
     def setData(self, **kwds):
         self.data = kwds
@@ -197,3 +264,54 @@ class FanCurveGraph(pg.GraphItem):
         
         self.updateGraph()
         event.accept()
+
+class ScrollingGraph(pg.GraphItem):
+    def __init__(self, parent):
+        pg.GraphItem.__init__(self)
+
+        InitPlotWidget(parent)
+
+        self.plotWidget = parent
+        self.name = 'graph'
+
+        data = np.random.normal(size=300)
+
+        parent.addItem(self)
+        self.plot = parent.plot(data)
+        self.pointer = 0
+
+    def update(self, y):
+        self.data[:-1] = self.data[1:]
+        self.data[-1] = y
+        self.pointer += 1
+        self.plot.setData(self.data)
+        self.plot.setPos(self.pointer, 0)
+        
+# # 1) Simplest approach -- update data in the array such that plot appears to scroll
+# #    In these examples, the array size is fixed.
+# p2 = win.addPlot()
+# data1 = np.random.normal(size=300)
+
+# curve2 = p2.plot(data1)
+# ptr1 = 0
+# def update():
+#     global data1, curve1, ptr1
+#     data1[:-1] = data1[1:]  # shift data in the array one sample left
+#                             # (see also: np.roll)
+#     data1[-1] = np.random.normal()
+    
+#     ptr1 += 1
+#     curve2.setData(data1)
+#     curve2.setPos(ptr1, 0)
+
+# timer = pg.QtCore.QTimer()
+# timer.timeout.connect(update)
+# timer.start(50)
+
+
+
+# ## Start Qt event loop unless running in interactive mode or using pyside.
+# if __name__ == '__main__':
+#     import sys
+#     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+#         QtGui.QApplication.instance().exec_()
