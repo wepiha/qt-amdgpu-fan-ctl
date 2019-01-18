62d62989b82591368f03030afffb8dfd5ed678bd
wepiha <2267001+wepiha@users.noreply.github.com>
2019-01-18 21:23:39 +1300

    Initial Commit

---
+++ common/theme.py
@@ -0,0 +1,18 @@
+
+
+
+BG_COLOR_MANUAL = "#ff5d00"
+BG_COLOR_AUTO = "#0000ff"
+
+UI_QLABEL_BG_CSS = "QLabel { \
+    color: white; \
+    background-color: %s \
+}"
+
+UI_DARK_ROUND_CSS = "QWidget { \
+    background-color: %s; \
+    border-style: solid; \
+    border-color: %s; \
+    border-width: 2px; \
+    border-radius: 3px; \
+}"
